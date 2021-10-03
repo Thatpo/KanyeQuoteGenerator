6a86b43f75adfb9fc14c2bfad9753cb5a1a5f16d
Thatpo <thatos1234@gmail.com>
2021-10-03 01:28:18 -0400

    adding tweet button functionality

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const text = document.getElementsByClassName("text")[0];
 const newQuote = document.getElementById("nw-qs");
-const tweet = document.getElementById("nw-tweet");
-
+const tweets = document.getElementById("tweet");
+let twitter = "";
 newQuote.addEventListener("click", function () {
     fetchQuotes()
+
 })
 
 async function fetchQuotes() {
     const resp = await fetch(`https://api.kanye.rest`)
     const data = await resp.json();
+    twitter = data.quote
     text.textContent = data.quote;
+
 }
 
-tweet.addEventListener("click", function () {
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${twitter}`;
+    window.open(twitterUrl, '_blank');
+}
+tweets.addEventListener("click", tweetQuote);
 
-})
